3c427bbfa4b26593373642ea2e705eb759956dc9
bkc-tomi <bkc.tomi1979@gamil.com>
2021-03-15 21:16:11 +0900

    change

--- starter/views.py
+++ starter/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     response = HttpResponse("started")
-    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     return response
